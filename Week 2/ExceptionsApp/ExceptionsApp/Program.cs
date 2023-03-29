8bbd9ea27f8ea78a4a6579ed0d818f7cdb53e10b
coffeeandcodee <a9idris@gmail.com>
2023-03-29 13:51:18 +0100

    Exceptions Lesson

---
+++ Week 2/ExceptionsApp/ExceptionsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionsApp;
+
+public class Program
+{
+    static void Main()
+    {
+        var text = File.ReadAllText("C://Users//Ahmed//OneDrive//Documents//Sparta Global//Tech211//Week 2//TypesOfErrors.txt");
+        Console.WriteLine(text);
+    }
+}
