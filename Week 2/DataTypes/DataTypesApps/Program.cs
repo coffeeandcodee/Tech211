8ab6d3c1cf7770a1fe2db8c6a1892c149c401224
coffeeandcodee <a9idris@gmail.com>
2023-03-30 10:44:34 +0100

    Added data types lessons

---
+++ Week 2/DataTypes/DataTypesApps/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataTypesApps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
