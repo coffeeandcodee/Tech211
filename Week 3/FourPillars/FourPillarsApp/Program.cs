f0643049481de4cf41be21c4fe9a79c234010e02
coffeeandcodee <a9idris@gmail.com>
2023-04-05 10:48:19 +0100

    Four Pillars

---
+++ Week 3/FourPillars/FourPillarsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FourPillarsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
