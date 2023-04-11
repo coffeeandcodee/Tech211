991e00302cb381761c24464baaa4f0dd3dbe1f9e
coffeeandcodee <a9idris@gmail.com>
2023-04-11 14:55:05 +0100

    Object Equality and Comparison

---
+++ Week 3/ObjectEqualityAndComparison/Person.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+
+namespace ObjectEqualityAndComparison;
+
+public class Person : IEquatable<Person?>, IComparable<Person>
+{
+    private string _firstName = "";
+    private string _lastName = "";
+
+    public int Age { get; set; }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        Age = age;
+        _firstName = firstName;
+        _lastName = lastName;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as Person);
+    }
+
+    public bool Equals(Person? other)
+    {
+        return other is not null &&
+               _firstName == other._firstName &&
+               _lastName == other._lastName &&
+               Age == other.Age;
+    }
+
+    public override int GetHashCode()
+    {
+        //A HashCode is a function that returns...
+        return HashCode.Combine(_firstName, _lastName, Age);
+    }
+
+    public int CompareTo(Person? other)
+    {
+        
+        if (other is null) return 1;
+        if (other.Age == Age)
+        {
+            if (other._lastName == _lastName)
+            {
+                if (other._firstName == _firstName) return 0;
+                else return _firstName.CompareTo(other._firstName);
+            }
+            else return _lastName.CompareTo(other._lastName);
+        }
+        else return -1 * Age.CompareTo(other.Age);
+
+    }
+
+    public string GetFullName()
+    {
+        return $"{_firstName} {_lastName}";
+    }
+
+    public static bool operator == (Person left, Person right)
+    {
+        return EqualityComparer<Person>.Default.Equals(left, right);
+    }
+
+    public static bool operator != (Person left, Person right)
+    {
+        return !(left == right);
+    }
+
+
+
+
+
+}
+
+
---
+++ Week 3/ObjectEqualityAndComparison/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ObjectEqualityAndComparison;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var andrew = new Person("andy", "Thomas", 9);
+        var phil  = new Person("Phil", "Thomas", 2);
+
+        Console.WriteLine("Andrew and phil are equal?" + andrew.Equals(phil));  //comparing location in memory in HEAP
+
+
+        var andrewClone = andrew;
+        Console.WriteLine(andrew.Equals(andrewClone)); //true
+
+        List<int> list = new() { 5, 1, 7, 99, 33 };
+        list.Sort();
+
+        foreach(int n in list) Console.WriteLine(n);
+
+        List<Person> people = new() { andrew, phil, andrewClone, new Person("Talal", "Hassan", 5), new Person("Danyal", "Saleh", 5000) };
+        people.Sort();
+
+        foreach(var p in people)
+        {
+            Console.WriteLine(p.GetFullName());
+        }
+
+    }
+}
